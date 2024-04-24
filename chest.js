74bfaed889367f3f299f073dc10d4a596b4ffe76
fabbe435 <fabian.wallin@skola.taby.se>
2024-04-25 00:18:21 +0200

    lite fix

---
+++ chest.js
@@ -0,0 +1,22 @@
+//variabler:
+const apiKey = '2xIvmDUVOpjG+4QgFWrHjg==7aIYVY0JpsxMQ8se';
+const muscle = 'chest'
+
+//fetch:
+fetch(`https://api.api-ninjas.com/v1/exercises?muscle=${muscle}`, {
+  headers: {
+    'X-Api-Key': '2xIvmDUVOpjG+4QgFWrHjg==7aIYVY0JpsxMQ8se'
+  }
+})
+.then(response => {
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+  return response.json();
+})
+.then(data => {
+  console.log(data);
+})
+.catch(error => {
+  console.error('Request failed:', error);
+});
