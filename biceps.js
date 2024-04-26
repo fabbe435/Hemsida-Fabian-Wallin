767b6da0a699a52c91f9118c8c3ee131f0380857
fabbe435 <fabian.wallin@skola.taby.se>
2024-04-26 15:11:35 +0200

    grejer massa grejs

--- biceps.js
+++ biceps.js
@@ -17,7 +17,37 @@ fetch(`https://api.api-ninjas.com/v1/exercises?muscle=${muscle}`, {
 })
 .then(data => {
   console.log(data);
+  if (data){
+    const exercises = data;
+    displayResults(exercises)
+  }
+  else{
+    console.error('Request failed:', error);
+  }
 })
-.catch(error => {
-  console.error('Request failed:', error);
-});
+
+
+function displayResults(exercises) {
+  const resultsSection = document.getElementById('resultsSection');
+  resultsSection.innerHTML = '';
+
+  exercises.forEach(exercise => {
+    const exerciseName = exercise.name || 'Namn saknas';
+    const exerciseType = exercise.type || 'Namn saknas';
+    const exerciseDescription = exercise.instructions || 'Ingen beskrivning tillagd';
+    const exerciseEquipment = exercise.equipment || 'Ingen information given';
+    const exerciseDifficulty = exercise.difficulty || 'Ingen information given';
+
+    const resultDiv = document.createElement('div');
+    resultDiv.classList.add('result');
+    resultDiv.innerHTML = `
+    <h3>${exerciseName}</h3>
+    <p>Type of exercise: ${exerciseType}</p>
+    <p>Description: ${exerciseDescription}</p>
+    <p>Equipment: ${exerciseEquipment}</p>
+    <p>Difficulty: ${exerciseDifficulty}</p>
+    `;
+
+    resultsSection.appendChild(resultDiv);
+  })
+}
--- chest.js
+++ chest.js
@@ -1,3 +1,4 @@
+
 //variabler:
 const apiKey = '2xIvmDUVOpjG+4QgFWrHjg==7aIYVY0JpsxMQ8se';
 const muscle = 'chest'
@@ -16,7 +17,49 @@ fetch(`https://api.api-ninjas.com/v1/exercises?muscle=${muscle}`, {
 })
 .then(data => {
   console.log(data);
+  if (data){
+    const exercises = data;
+    displayResults(exercises)
+  }
+  else{
+    console.error('Request failed:', error);
+  }
 })
-.catch(error => {
-  console.error('Request failed:', error);
-});
+
+function displayText(){
+  var text = document.getElementById("textField");
+  text.style.display = "block";
+  if (text.style.display ="block"){
+  }
+}
+
+function hideText(){
+  var text = document.getElementById("TextField");
+  text.style.display = "none";
+}
+
+function displayResults(exercises) {
+  const resultsSection = document.getElementById('resultsSection');
+  resultsSection.innerHTML = '';
+
+  exercises.forEach(exercise => {
+    const exerciseName = exercise.name || 'Namn saknas';
+    const exerciseType = exercise.type || 'Namn saknas';
+    const exerciseInstructions = exercise.instructions || 'Ingen beskrivning tillagd';
+    const exerciseEquipment = exercise.equipment || 'Ingen information given';
+    const exerciseDifficulty = exercise.difficulty || 'Ingen information given';
+
+    const resultDiv = document.createElement('div');
+    resultDiv.classList.add('result');
+    resultDiv.innerHTML = `
+    <h3>${exerciseName}</h3>
+    <p>Difficulty: ${exerciseDifficulty}</p>
+    <p>Type of exercise: ${exerciseType}</p>
+    <p>Equipment: ${exerciseEquipment}</p>
+    <button id="DisplayInstructions" onclick="displayText()">Show/Hide Instructions</button>
+    <div id="textField">Instructions: ${exerciseInstructions}</div>
+    `;
+
+    resultsSection.appendChild(resultDiv);
+  })
+}
